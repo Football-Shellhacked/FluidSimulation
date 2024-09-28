4f867c052ea16e04011c17038bad26aeb31cc5d6
SamuelD102003 <samdiez2003@gmail.com>
2024-09-28 16:20:56 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,94 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-For a C++ project simply rename the file to .cpp and re-run the build script 
-
--- Copyright (c) 2020-2024 Jeffery Myers
---
---This software is provided "as-is", without any express or implied warranty. In no event 
---will the authors be held liable for any damages arising from the use of this software.
-
---Permission is granted to anyone to use this software for any purpose, including commercial 
---applications, and to alter it and redistribute it freely, subject to the following restrictions:
-
---  1. The origin of this software must not be misrepresented; you must not claim that you 
---  wrote the original software. If you use this software in a product, an acknowledgment 
---  in the product documentation would be appreciated but is not required.
---
---  2. Altered source versions must be plainly marked as such, and must not be misrepresented
---  as being the original software.
---
---  3. This notice may not be removed or altered from any source distribution.
-
-*/
-
 #include "raylib.h"
 #include "Particle.h"
 #include <Engine.h>
-
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include <stdlib.h>
+#include <stdio.h>
+#include "resource_dir.h"
 
 extern Particle particles[NUM_PARTICLES];
 
 int main ()
 {
-	sayHello();
-	// Tell the window to use vysnc and work on high DPI displays
+	int globalparticlecount = NUM_PARTICLES;
+
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+	InitWindow(WORLD_BOUND_X, WORLD_BOUND_Y, "Liquid Simulation");
 
-	// Create the window and OpenGL context
-	InitWindow(WORLD_BOUND_X, WORLD_BOUND_Y, "Hello Raylib");
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+
+	char name[5] = "\0";
+    int letterCount = 0;
+    Rectangle textBox = { 10, 10, 110, 50 };
+    bool mouseOnText = false;
+    int framesCounter = 0;
+
+
+
 	SearchAndSetResourceDir("resources");
-
-	// Load a texture from the resources directory
 	GenerateChunks(6,3);
 	GenerateParticles();
 	
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	while (!WindowShouldClose())		
 	{
-		// drawing
+
+
+
+		if (CheckCollisionPointRec(GetMousePosition(), textBox)) mouseOnText = true;
+        else mouseOnText = false;
+        if (mouseOnText)
+        {
+            SetMouseCursor(MOUSE_CURSOR_IBEAM);
+            int key = GetCharPressed();
+            while (key > 0)
+            {
+                
+                if ((key >= 48) && (key <= 57) && (letterCount < 4))
+                {
+                    name[letterCount] = (int)key;
+                    name[letterCount+1] = '\0';
+                    letterCount++;
+                }
+                key = GetCharPressed();
+            }
+            if (IsKeyPressed(KEY_BACKSPACE))
+            {
+                letterCount--;
+                if (letterCount < 0) letterCount = 0;
+                name[letterCount] = '\0';
+            }
+			else if (IsKeyPressed(KEY_SPACE))
+			{
+				int value = atoi(name);
+				letterCount = 0;
+				globalparticlecount = value;
+				printf("%d\n",globalparticlecount);
+				 name[letterCount] = '\0';
+
+			}
+        }
+        else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+        if (mouseOnText) framesCounter++;
+        else framesCounter = 0;
+
+
+
+
 		BeginDrawing();
-
-		// Setup the backbuffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
-
 		ProcessParticles();
-		// draw our texture to the screen
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+
+
+
+		DrawText("PLACE MOUSE OVER BOX TO CHANGE PARTICLE# AND PRESS SPACE TO CONFIRM", 130, 10, 20, GRAY);
+            DrawRectangleRec(textBox, LIGHTGRAY);
+            if (mouseOnText) DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, RED);
+            else DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, DARKGRAY);
+            DrawText(name, (int)textBox.x + 5, (int)textBox.y + 8, 40, RED);
+
+
 		EndDrawing();
 	}
 
-	// cleanup
-	// unload our texture so it can be cleaned u
-
-	// destory the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
 }
