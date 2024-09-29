6b23c095ba8bcd39e97561e1d3a83c8dfd0bd818
FatWrinkleZ <mgiraldo2003@gmail.com>
2024-09-28 21:47:02 -0400

    implemented better blur

--- resources/blur_horizontal.fs
+++ resources/blur_horizontal.fs
@@ -8,11 +8,9 @@ uniform float blurStrength;
 
 void main() {
     vec4 sum = vec4(0.0);
+    vec2 texOffset = vec2(blurStrength) / vec2(textureSize(texture0, 0)); // Texel offset
 
-    // Calculate texel offset based on blur strength and texture size
-    vec2 texOffset = vec2(blurStrength) / vec2(textureSize(texture0, 0)); 
-
-    // Gaussian weights for a 5x5 kernel
+    // Gaussian weights for a 5x1 kernel
     float weights[5] = float[](0.227027, 0.1945946, 0.1216216, 0.0540541, 0.016216);
 
     // Apply horizontal blur
@@ -20,11 +18,5 @@ void main() {
         sum += texture(texture0, fragTexCoord + vec2(texOffset.x * float(i), 0.0)) * weights[abs(i)];
     }
 
-    // Apply vertical blur
-    vec4 blurColor = vec4(0.0);
-    for (int j = -4; j <= 4; j++) {
-        blurColor += sum * weights[abs(j)];
-    }
-
-    fragColor = blurColor;
+    fragColor = sum;
 }
